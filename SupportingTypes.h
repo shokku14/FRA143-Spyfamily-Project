f42291d9610e8781f0a46f3f29a422078a914e44
Sho <kanrawee.koj@gmail.com>
2026-05-05 20:16:06 +0700

    Add SupportingTypes header file

--- SupportingTypes.h
+++ SupportingTypes.h
@@ -1,11 +1,13 @@
 #pragma once
 
+// กำหนดสถานะของเกม[cite: 2]
 enum class GameState {
     READY,
     RUNNING,
     FINISHED
 };
 
+// กำหนดประเภทของหมากทั้งหมดในเกม[cite: 2]
 enum class PieceType {
     SMALL,
     MEDIUM,
@@ -14,6 +16,7 @@ enum class PieceType {
     SPY
 };
 
+// กำหนดสีหรือฝ่ายของผู้เล่น[cite: 2]
 enum class PlayerColor {
     RED,
     BLUE
